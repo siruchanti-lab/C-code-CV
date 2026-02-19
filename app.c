67489fdafb3ef70c7c90b151dc92b040d2910502
Srichu-Codes <siruchanti@gmail.com>
2026-02-19 20:50:01 +0000

    Update code

--- app.c
+++ app.c
@@ -3,6 +3,7 @@
 void main()
 {
 int n;
+scanf("%d",&n);
 printf("%d raised to the power of 2 is %d",n,pow(n,2));
 printf("%d raised to the power of 3 is %d",n,pow(n,3));
 printf("%d raised to the power of 4 is %d",n,pow(n,4));
