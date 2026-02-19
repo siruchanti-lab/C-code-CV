d31132c32b682493232489e1617201e4da35bdf1
Srichu-Codes <siruchanti@gmail.com>
2026-02-19 20:47:53 +0000

    Add app.c

---
+++ app.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int n;
+printf("%d raised to the power of 2 is %d",n,pow(n,2));
+printf("%d raised to the power of 3 is %d",n,pow(n,3));
+printf("%d raised to the power of 4 is %d",n,pow(n,4));
+}
