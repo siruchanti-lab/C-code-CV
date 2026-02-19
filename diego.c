14f10aa32851246c8d10894b13904dff246f21fc
Srichu-Codes <siruchanti@gmail.com>
2026-02-19 20:55:07 +0000

    Fix format warning in diego.c

--- diego.c
+++ diego.c
@@ -2,5 +2,20 @@
 #include<math.h>
 void main()
 {
-    printf("")
+int n[5];
+printf("Enter 5 numbers:");
+for(int i=0;i<5;i++)
+{
+scanf("%d",&n[i]);
+}
+printf("The squares of the numbers are:\n");
+for(int i=0;i<5;i++)
+{
+printf("%f ",pow(n[i],2));
+}
+printf("\nThe square roots of the numbers are:\n");
+for(int i=0;i<5;i++)
+{
+printf("%f ",sqrt(n[i]));
+}
 }
