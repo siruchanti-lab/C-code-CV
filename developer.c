fb80eebc3b78dd28fd202d82f44f2b323ef9ad98
Srichu-Codes <siruchanti@gmail.com>
2026-02-24 16:03:53 +0000

    Save developer.c

---
+++ developer.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int i,lim,osum=0,esum=0;
+    printf("enter the limit\n");
+    scanf("%d",&lim);
+    for(i=0;i<=lim;i++)
+    {
+        if(i%2==0)
+        esum=esum+i;
+    else
+    osum=osum+i;
+    }
+    printf("the sum of even %d\n the sum of odd %d",esum,osum);
+}
