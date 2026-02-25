0bcbfaf76a9598bf495f8dcfeb3ff22c8c73df43
Srichu-Codes <262291356+siruchanti-lab@users.noreply.github.com>
2026-02-25 15:45:22 +0000

    commit and push it

---
+++ udf1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void perirect(int l, int b)
+{
+    printf("perimeter of the rectangle is %d",2(l+b));
+}
+void main()
+{
+    int a,b;
+    printf("enter the two values");
+    scanf("%d,%d",&a,&b);
+    perirect(a,b);
+}
