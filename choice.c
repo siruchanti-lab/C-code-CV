a52e0268604ae3c59e88562e944e9a5cb194bebf
Srichu-Codes <262291356+siruchanti-lab@users.noreply.github.com>
2026-02-25 16:27:18 +0000

    commit and push to github

--- choice.c
+++ choice.c
@@ -1,31 +1,33 @@
-#include<stdio.h.>
+#include<stdio.h>
 void add(int x, int y)
 {
     printf("The sum is %d",x+y);
-}
-void difference(int x, int y)
-{
-    printf("The difference is %d",x-y);
-}
-void product(int x, int y)
-{
-    printf("the product is %d",x*y);
-}
-void main()
-{
-    int a,b,choice;
-    printf("enter the choice")
-    scanf("%d",&choice);
-    switch(choice)
-    {
-        case choice=1:
-        add(a,b);
-        break;
-        case choice=2:
-        difference(a,b);
-        break;
-        case choice=3;
-        product(a,b);
-        break;    
     }
-}
+    void difference(int x, int y)
+    {
+        printf("The difference is %d",x-y);
+        }
+        void product(int x, int y)
+        {
+            printf("the product is %d",x*y);
+            }
+            void main()
+            {
+                int a,b,choice;
+                    printf("enter the choice");
+                        scanf("%d",&choice);
+                        printf("enter the values of a and b");
+                        scanf("%d,%d",&a,&b);
+                            switch(choice)
+                                {
+                                        case 1:
+                                                add(a,b);
+                                                        break;
+                                                                case 2:
+                                                                        difference(a,b);
+                                                                                break;
+                                                                                        case 3:
+                                                                                                product(a,b);
+                                                                                                        break;    
+                                                                                                            }
+                                                                                                            }
